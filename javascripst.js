6e5832983dce39ec438c7433c800333484519fae
¨Jane <smoreira11@hotmail.com>
2025-04-23 22:27:01 +0900

    add japanese and finish

--- javascripst.js
+++ javascripst.js
@@ -6,8 +6,8 @@ const tooltip = d3.select("#tooltip");
 
 const path = d3.geoPath();
 const colorScale = d3.scaleThreshold()
-  .domain([10, 20, 30, 40])
-  .range(["#f2f0f7", "#cbc9e2", "#9e9ac8", "#6a51a3", "#4a1486"]);
+  .domain([10, 20, 30, 40, 50, 60])
+  .range(["#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#3182bd", "#08519c"]);
 
 Promise.all([
   d3.json(countiesURL),
@@ -36,21 +36,51 @@ Promise.all([
         .attr("data-education", edu?.bachelorsOrHigher || 0)
         .html(`
           <strong>${edu?.area_name}, ${edu?.state}</strong><br>
-          ${edu?.bachelorsOrHigher}% con educación universitaria
+          ${edu?.bachelorsOrHigher}% が大学教育を受けた
         `);
     })
     .on("mouseout", () => tooltip.style("opacity", 0));
 
-  // Leyenda
-  const legend = d3.select("#legend");
+   // プロフェッショナルなSVG凡例
+const legendWidth = 300;
+const legendHeight = 10;
+const legendColors = colorScale.range();
+const legendThresholds = colorScale.domain();
 
-  const legendScale = [0, 10, 20, 30, 40];
-  const legendColors = colorScale.range();
+const legendSvg = d3.select("#legend");
 
-  legend.selectAll("div")
-    .data(legendColors)
-    .enter()
-    .append("div")
-    .attr("class", "legend-item")
-    .style("background-color", d => d);
+const xScale = d3.scaleLinear()
+  .domain([d3.min(legendThresholds), d3.max(legendThresholds)])
+  .range([0, legendWidth]);
+
+// メイングループを作成
+const legendGroup = legendSvg.append("g")
+  .attr("transform", "translate(40,20)");
+
+// カラーブロックを作成
+legendGroup.selectAll("rect")
+  .data(legendColors)
+  .enter()
+  .append("rect")
+  .attr("x", (d, i) => xScale(legendThresholds[i] || 0))
+  .attr("y", 0)
+  .attr("width", (d, i) => {
+    const next = legendThresholds[i + 1] || xScale.domain()[1];
+    return xScale(next) - xScale(legendThresholds[i] || 0);
+  })
+  .attr("height", legendHeight)
+  .attr("fill", d => d);
+
+// 値付き軸
+const xAxis = d3.axisBottom(xScale)
+  .tickSize(13)
+  .tickValues(legendThresholds)
+  .tickFormat(d => d + "%");
+
+legendGroup.append("g")
+  .attr("transform", `translate(0,${legendHeight})`)
+  .call(xAxis)
+  .select(".domain")
+  .remove();
+
 });
